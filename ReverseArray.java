296117e223b90eb6e591b2fc4f8df31ebf292f9c
Ash <47375278+ash2983@users.noreply.github.com>
2022-03-01 19:19:23 -0600

    Create ReverseArray.java

---
+++ ReverseArray.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    // O(n) solution complexity 
+    public static List<Integer> reverseArray(List<Integer> a) {
+        
+        List<Integer> reversed = new ArrayList(); //reversed list to be eventually returned
+        int index, rindex; //array index, reversed array index
+        
+        //loop through provided array backwards and add to reversed array
+        for(index = a.size() - 1, rindex = 0; index >= 0; index--, rindex++){
+                reversed.add(a.get(index)); 
+        }
+
+        //return reversed array
+        return reversed; 
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> res = Result.reverseArray(arr);
+
+        bufferedWriter.write(
+            res.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
